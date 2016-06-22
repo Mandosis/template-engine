2a608fd705b54d7e62201dfb9cdb8fe4475dc7bb
Chris Rabuse <rab233@live.com>
2016-06-22 10:53:39 -0500

    Add tests for rendering

--- test/index.js
+++ test/index.js
@@ -3,8 +3,25 @@ var path = require('path');
 var template = require('../lib/index');
 
 describe('Render', () => {
+  let contents = '';
+  let filePath = path.join(__dirname, './cases/example.tpl');
   it('Reads the file', () => {
-    template.readFile(path.join(__dirname, './cases/example.tpl'), (err, data) => {
+    template.readFile(filePath, (err, data) => {
+      should.not.exist(err);
+      data.should.be.a('string');
+      contents = data;
+    })
+  })
+
+  it('Parses the file', () => {
+    template.parseFile(contents, filePath, (err, data) => {
+      should.not.exist(err);
+      data.should.be.a('string')
+    })
+  })
+
+  it('Renders the file to HTML', () => {
+    template.render(filePath, {}, (err, data) => {
       should.not.exist(err);
       data.should.be.a('string');
     })
